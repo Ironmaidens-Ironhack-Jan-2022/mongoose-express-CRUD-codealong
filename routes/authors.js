31acfad2925040330d83ef92f2654e7113135f17
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-02-08 17:57:08 +0100

    display numberOfBooks in the list of authors

--- routes/authors.js
+++ routes/authors.js
@@ -1,11 +1,29 @@
 const router = require("express").Router();
 const Author = require("../models/Author.model");
+const Book = require("../models/Book.model");
 
 
 router.get("/", (req, res, next) => {
-    Author.find()
-        .then(authorsFromDB => {
-            res.render("authors/authors-list", {authors: authorsFromDB});
+    
+    let books; // we create a variable in the parent scope
+
+    Book.find()
+        .then( (booksFromDB) => {
+            books = booksFromDB; // update our variable in the parent scope
+            return Author.find()
+        })
+        .then( authorsFromDB => {
+            const authorsArr = authorsFromDB.map( (authorDetails) => {
+                const {_id, name, age, country} = authorDetails;
+                const booksFromCurrentAuthor = books.filter( book => {
+                    return book.author.toString() == _id.toString(); // compare ids as string
+                })
+                const numberOfBooks = booksFromCurrentAuthor.length;
+
+                return {name, age, country, numberOfBooks};
+            })
+
+            res.render("authors/authors-list", {authors: authorsArr});
         })
         .catch(err => {
             console.log('Error getting authors from DB...', err);
