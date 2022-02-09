13bd248d328d9c126e4bcb74a58f48afa477c67d
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-02-09 18:10:17 +0100

    store session data in DB with connect-mongo

--- config/session.config.js
+++ config/session.config.js
@@ -1,4 +1,6 @@
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const mongoose = require('mongoose');
 
 // since we are going to USE this middleware in the app.js,
 // let's export it and have it receive a parameter
@@ -9,8 +11,7 @@ module.exports = app => {
 
   // required for the app when deployed to Heroku (in production)
   app.set('trust proxy', 1);
-
-  // use session
+  
   app.use(
     session({
       secret: process.env.SESS_SECRET,
@@ -20,8 +21,13 @@ module.exports = app => {
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        maxAge: 60000 // 60 * 1000 ms === 1 min
-      }
+        maxAge: 60000
+      },
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/library-project'
+        // ttl => time to live
+        // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
+      })
     })
   );
 };
