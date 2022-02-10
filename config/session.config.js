83e854e1baa95202e4386b853c84c699f58e4074
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-02-10 12:12:10 +0100

    config session ttl

--- config/session.config.js
+++ config/session.config.js
@@ -24,9 +24,8 @@ module.exports = app => {
         maxAge: 60000
       },
       store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/library-project'
-        // ttl => time to live
-        // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/library-project',
+        ttl: 60 * 60 // 60sec * 60min * 24h => 1 day
       })
     })
   );
